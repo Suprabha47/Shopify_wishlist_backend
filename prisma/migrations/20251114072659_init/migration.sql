788d3272c47345466a7cc01068feb41730d7186b
Divyansh Sharma <divyansh.sharma@iamtechie.com>
2025-11-14 12:57:44 +0530

    database_settings_update

---
+++ prisma/migrations/20251114072659_init/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `collectionIconFillColor` on the `ShopSettings` table. All the data in the column will be lost.
+  - You are about to drop the column `iconFillColor` on the `ShopSettings` table. All the data in the column will be lost.
+  - You are about to drop the column `iconStyle` on the `ShopSettings` table. All the data in the column will be lost.
+  - You are about to drop the column `productIconFillColor` on the `ShopSettings` table. All the data in the column will be lost.
+  - You are about to drop the column `showStock` on the `ShopSettings` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ShopSettings" DROP COLUMN "collectionIconFillColor",
+DROP COLUMN "iconFillColor",
+DROP COLUMN "iconStyle",
+DROP COLUMN "productIconFillColor",
+DROP COLUMN "showStock";
